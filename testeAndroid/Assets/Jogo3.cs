b8702b271d663e19fd5cf1fd01f57c228e991b42
RobertoOliveiira <roberto.oliveira1999@hotmail.com>
2020-12-15 20:51:14 -0300

    Finalizado

---
+++ testeAndroid/Assets/Jogo3.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Jogo3 : MonoBehaviour
+{
+    public GameObject painel; 
+    public GameObject botao;
+    public GameObject[] lixos;
+    public GameObject porta1, porta2;
+    System.Random rand = new System.Random();
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject fo in lixos)
+        {
+            fo.GetComponent<Rigidbody2D>().gravityScale = (float)rand.NextDouble() * (1 - (-1)) + (-1) ;
+        }
+        botao.GetComponent<Button>().onClick.AddListener(delegate{clicou();});
+
+    }
+
+    public IEnumerator contador(){
+        yield return new WaitForSeconds(2.0f);
+        porta1.SetActive(true);
+        porta2.SetActive(true);
+        foreach (GameObject fo in lixos)
+        {
+            fo.GetComponent<Rigidbody2D>().gravityScale = (float)rand.NextDouble() * (1 - (-1)) + (-1) ;
+        }
+        painel.SetActive(true);
+        botao.transform.Rotate(0 , 0, 180);
+        GameObject.Find("Jogo 3").gameObject.SetActive(false);
+    }
+
+    void clicou(){
+        botao.transform.Rotate(0 , 0, 180);
+        porta1.SetActive(false);
+        porta2.SetActive(false);
+        foreach (GameObject fo in lixos)
+        {
+            fo.GetComponent<Rigidbody2D>().gravityScale = 689;
+        }
+        StartCoroutine(contador());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ testeAndroid/Assets/Script/QRCodeLer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.QrCode;
+
+public class QRCodeLer : MonoBehaviour
+{
+    [SerializeField] private Texture2D qrCodeTexture;
+    [SerializeField] private RawImage qrCodeRawImage;
+    [SerializeField] private Text qrCode_Resultado;
+
+    public void LerQRcode()
+    {
+        try
+        {
+            Texture2D qrCodeTexture = qrCodeRawImage.texture as Texture2D;
+            IBarcodeReader barcodeReader = new BarcodeReader();
+            var result = barcodeReader.Decode(qrCodeTexture.GetPixels32(),qrCodeTexture.width,qrCodeTexture.height);
+            if (result != null)
+            {
+                qrCode_Resultado.text = result.Text;
+                Debug.Log("qrCode_Resultado: " +result.Text);
+            }
+        }
+        catch (System.Exception ex) { Debug.LogWarning(ex.Message); }
+    }
+
+}
